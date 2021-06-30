1d73a93d3349b8a030fae75c34af72f9b66a3fe8
Alec Venzor <alecvenzordev@gmail.com>
2021-06-30 12:18:49 -0700

    fixed mobile

--- Gatsby/src/components/MobileDrop.js
+++ Gatsby/src/components/MobileDrop.js
@@ -15,7 +15,7 @@ const Menue = styled.div`
 const StyledImg = styled(GatsbyImage)`
   width: 15%;
   margin: 10px 24px;
-  z-index: 50;
+  z-index: 51;
   :nth-child(2) {
     width: 25px;
     height: 25px;
@@ -25,16 +25,26 @@ const StyledImg = styled(GatsbyImage)`
 const Nav = styled.nav`
   display: ${(props) => (props.open ? "flex" : "none")};
   height: 100vh;
-  z-index: 10;
-  justify-content: center;
+  z-index: 49;
+  padding-top: 20%;
   flex-flow: column nowrap;
-  align-content: space-around;
+  align-content: center;
   position: absolute;
   width: 100%;
   height: 30vh;
-  background: gray;
+  background: black;
   justify-content: space-around;
 `;
+const Icon = styled(GatsbyImage)`
+  margin-right: 24px;
+  margin-left: 5%;
+`;
+const A = styled.a`
+  text-decoration: none;
+  display: flex;
+  flex-flow: row nowrap;
+`;
+const P = styled.p``;
 const MobileDrop = () => {
   const data = useStaticQuery(graphql`
     {
@@ -88,9 +98,10 @@ const MobileDrop = () => {
       />
       <Nav open={open}>
         {data.allSanityMetaData.edges[0].node.socialMedia.map((res) => (
-          <a href={res.url} key={res.url}>
-            {res.text}
-          </a>
+          <A href={res.url} key={res.url}>
+            <Icon image={res.favcon.asset.gatsbyImageData} alt={res.alt} />
+            <P>{res.text}</P>
+          </A>
         ))}
       </Nav>
     </Menue>
