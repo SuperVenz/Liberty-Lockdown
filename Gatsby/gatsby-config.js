d0291cda8b0ac537aafefb30afaf75d31bcc0d2d
Alec Venzor <alecvenzordev@gmail.com>
2021-07-04 13:33:05 -0700

    cutt offline support

--- Gatsby/gatsby-config.js
+++ Gatsby/gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
 
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         start_url: `/`,
         icon: "src/images/icon.png",
+        cache_busting_mode: "none",
         crossOrigin: `anonymous`, // `use-credentials` or `anonymous`
         display: `standalone`,
         icon: "src/images/icon.png",
@@ -37,12 +37,6 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
-    },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/*`],
-      },
     },
   ],
 };
